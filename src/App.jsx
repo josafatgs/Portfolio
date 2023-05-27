e8f8565983f3c60145a333365997f09c789d6abd
Josafat <josafatgarciasarmientos@gmail.com>
2023-05-26 23:40:26 -0600

    Project Component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-        <section className={"bg-black min-h-screen pt-12"}>
+        <section className={"bg-black min-h-screen pt-12"} id={"inicio"}>
             <h1 className={"text-white pb-8"}>
                 Soy Josafat, un geek apasionado de la programación. Me encanta sumergirme en líneas de código y crear soluciones sorprendentes. Mi mente curiosa siempre busca nuevos desafíos para superar y llevar mi habilidad al siguiente nivel.
             </h1>
@@ -16,7 +16,11 @@ function App() {
             </h2>
         </section>
 
-        <Project projectId={"project-one"} projectName={"Proyecto Numero Uno"} projectImageSrc={"#"} />
+        <Project projectId={"project-one"} projectName={"Proyecto Numero Uno"} projectImageSrc={"https://picsum.photos/750/400"} nextProject={"project-two"} />
+
+        <Project projectId={"project-two"} projectName={"Proyecto Numero Dos"} projectImageSrc={"https://picsum.photos/750/400"} nextProject={"project-three"} />
+
+        <Project projectId={"project-three"} projectName={"Proyecto Numero Tres"} projectImageSrc={"https://picsum.photos/750/400"} nextProject={""} />
 
     </>
   );
--- src/Project.jsx
+++ src/Project.jsx
@@ -1,11 +1,21 @@
 
-function Project({projectId, projectName, projectImageSrc} ) {
+function Project({projectId, projectName, projectImageSrc, nextProject} ) {
+
   return (
-      <section id={projectId} className={"bg-black min-h-screen pt-12 pb-10"}>
-        <h3 className={"text-8xl"}>
-          {projectName}
-        </h3>
-        <img src={projectImageSrc} alt={"Imagen de Proyecto"}/>
+      <section id={projectId} className={"bg-black min-h-screen pt-12 pb-10 grid relative"}>
+
+          <h3 className={"text-8xl text-blue-950 pb-10"}>
+            {projectName}
+          </h3>
+          <img src={projectImageSrc} alt={"Imagen de Proyecto"} className={"place-self-center"} />
+          <a href={"#"}>Ver</a>
+
+          {
+              nextProject ?
+              <a href={`#${nextProject}`} >Siguiente Proyecto <i>&darr;</i></a> :
+              <a href={"Inicio"} > Volver al Inicio</a>
+          }
+
       </section>
   );
 }
